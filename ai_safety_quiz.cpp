1a3c2dadc314ddde64a63942b42c907af4629d23
Shweta Singh <shwetasingh5420@gmail.com>
2026-06-07 11:53:04 +0530

    Create ai_safety_quiz.cpp

---
+++ ai_safety_quiz.cpp
@@ -0,0 +1,90 @@
+#include <iostream>  
+using namespace std;
+
+int main() {
+
+    char answer;      // Store user's answer from taking user
+    int score = 0;    // Store quiz score
+
+   
+    cout << "=================================\n";
+    cout << "       AI SAFETY QUIZ\n";
+    cout << "=================================\n\n";
+
+    // Question 1
+    cout << "Q1. What does AI stand for?\n";
+    cout << "A. Artificial Intelligence\n";
+    cout << "B. Automatic Internet\n";
+    cout << "C. Artificial Internet\n";
+    cout << "D. Advanced Information\n";
+
+    cout << "Enter your answer: ";
+    cin >> answer;
+
+    // Check the answer correct or not
+    if(answer == 'A' || answer == 'a'){
+        cout << "Correct!\n\n";
+        score++; // Add 1 point
+    }
+    else{
+        cout << "Wrong! Correct answer is A.\n\n";
+    }
+
+    // Question 2
+    cout << "Q2. Why is AI Safety important?\n";
+    cout << "A. To make AI safe for people\n";
+    cout << "B. To make AI colorful\n";
+    cout << "C. To make computers bigger\n";
+    cout << "D. None of these\n";
+
+    cout << "Enter your answer: ";
+    cin >> answer;
+
+    // Check the answer
+    if(answer == 'A' || answer == 'a'){
+        cout << "Correct!\n\n";
+        score++;
+    }
+    else{
+        cout << "Wrong! Correct answer is A.\n\n";
+    }
+
+    // Question 3
+    cout << "Q3. Which company created ChatGPT?\n";
+    cout << "A. Google\n";
+    cout << "B. Microsoft\n";
+    cout << "C. OpenAI\n";
+    cout << "D. Apple\n";
+
+    cout << "Enter your answer: ";
+    cin >> answer;
+
+    // Check the answer
+    if(answer == 'C' || answer == 'c'){
+        cout << "Correct!\n\n";
+        score++;
+    }
+    else{
+        cout << "Wrong! Correct answer is C.\n\n";
+    }
+
+    // return final score
+    cout << "=================================\n";
+    cout << "Quiz Completed!\n";
+    cout << "Your Score: " << score << "/3\n";
+
+    // Display result message
+    if(score == 3){
+        cout << "Excellent! AI Safety Champion.\n";
+    }
+    else if(score >= 2){
+        cout << "Good Job! Keep Learning.\n";
+    }
+    else{
+        cout << "Keep Practicing.\n";
+    }
+
+    cout << "=================================\n";
+
+    return 0; // End of program
+}
